857b6514c51068bc8096769c68ffaf379fa516e1
marcik357 <marcik357@gmail.com>
2023-01-19 14:59:19 +0200

    add second task

--- JS-9/js/script.js
+++ JS-9/js/script.js
@@ -78,3 +78,47 @@ function resetTimer() {
     minutes.innerText = '00';
     stopwatchDisplay.classList.add('silver');
 }
+//==================================================================================
+
+
+
+// Реалізуйте програму перевірки телефону
+// * Використовуючи JS Створіть поле для введення телефону та кнопку збереження
+// * Користувач повинен ввести номер телефону у форматі 000-000-00-00
+// * Після того як користувач натискає кнопку зберегти перевірте правильність введення номера, якщо номер
+// правильний
+// зробіть зелене тло і використовуючи document.location переведіть користувача на сторінку
+// https://risovach.ru/upload/2013/03/mem/toni-stark_13447470_big_.jpeg
+// якщо буде помилка, відобразіть її в діві до input.
+// Insert input and button
+document.querySelector('.container-stopwatch').insertAdjacentHTML('afterend', `
+    <input type='text' class='input-phone' placeholder='000-000-00-00'>
+    <button class='save-phone'>Зберегти</button>
+`);
+// Variables
+const input = document.querySelector('.input-phone');
+const saveBtn = document.querySelector('.save-phone');
+const regExp = /\d\d\d\-\d\d\d-\d\d-\d\d/
+// Ban all input exept number and '-'
+input.addEventListener('beforeinput', (e) => {
+    if (isNaN(e.data) && e.data !== '-') {
+        e.preventDefault();
+    }
+});
+input.addEventListener('focus', () => input.classList.remove('green'));
+// Add listener on button
+saveBtn.addEventListener('click', submitePhone);
+// Check input correct
+function submitePhone(e) {
+    if (regExp.test(input.value)) {
+        input.classList.add('green');
+        setTimeout(() => {
+            document.location = 'https://i.picsum.photos/id/724/5000/3333.jpg?hmac=ZBbc0s-KVshJfX3Z8f9V-_djOHr3C9gudYkWDFVL6GM'
+        }, 1000);
+    } else {
+        input.insertAdjacentHTML('beforebegin', `
+        <img src='https://i.picsum.photos/id/685/3000/2000.jpg?hmac=GLauGOEYjS7xOFx_wgC1vpX7_QRZNjtK4Dk-1OCZ1BI'>
+        `);
+    }
+}
+//==================================================================================
