11f57a19fe9f3e1f1cc50f109a48563931a669d6
marcik357 <marcik357@gmail.com>
2023-03-07 14:54:39 +0200

    Update: functions.js - add genId fn, add createInputString, add createEditProductInput, minor change createElem

--- JS-14__itea0410crm/code/functions.js
+++ JS-14__itea0410crm/code/functions.js
@@ -1,46 +1,93 @@
+import { changeCategoryEvent } from "./events.js";
+
 export const validate = (p, v) => p.test(v);
+export const generationId = () => {
+    const sizeID = Math.floor(Math.random() * (10 - 18) + 18)
+    const a = "qwertyuiopasdfghjklzxcvbnm1234567890$&";
+    let r = "";
 
-/*
-export class CreateHTMLElement {
-    constructor(tagName = "div", className, value, attr = []) {
-        el = this;
-        el = document.createElement(tagName);
-        if (className !== undefined) {
-            el.classList.add(className)
-        }
-        if (tagName.includes("inputtextareaoption")) {
-            el.value = value;
-        } else {
-            el.innerText = value;
-        }
-        attr.forEach((attr) => {
-            if (typeof attr === "object") {
-                el.setAttribute(attr.props, attr.value)
-            }
-        })
+    for (let i = 0; i < sizeID; i++) {
+        r += a[Math.floor(Math.random() * a.length)];
     }
+
+    return r
 }
-*/
 
-export function createHTMLElement  (tagName = "div", className, value, attr = []) {
-        const  el = document.createElement(tagName);
-        if (className !== undefined) {
-            el.classList.add(className)
-        }
-        if (tagName.includes("inputtextareaoption")) {
+export function dateNow() {
+    return `${new Date().getFullYear()}/${new Date().getMonth()}/${new Date().getDate()} ${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()}`
+}
+
+export function categorySelect() {
+    const catigoty = document.querySelector(".catigoty");
+
+    catigoty.insertAdjacentHTML("beforeend",
+        `<select id="category">
+    <option value="" disabled selected>Оберіть категорію</option>
+    <option value="Магазин">Магазин</option>
+    <option value="Відео хостинг">Відео хостинг</option>
+    <option value="Рестаран">Рестаран</option>
+</select>`);
+
+    document.querySelector("#category")
+        .addEventListener("change", changeCategoryEvent)
+}
+
+
+export function createHTMLElement(tagName = "div", className, value, attr = [], listener) {
+    const el = document.createElement(tagName);
+    if (className) {
+        el.classList.add(className)
+    }
+    if ("inputtextareaoption".includes(tagName)) {
+        if (value) {
             el.value = value;
-        } else {
-            el.innerText = value;
         }
-        attr.forEach((attr) => {
+        if (listener) {
+            el.addEventListener("change", listener)
+        }
+    } else {
+        if (value !== undefined) {
+            el.innerHTML = value;
+        }
+        if (listener) {
+            el.addEventListener("click", listener)
+        }
+    }
+    attr.forEach(attr => {
+        // Зробити, щоб ми отримували точно сам об.
+        if (typeof attr === "object" && attr !== null) {
+            // отримати Значання з об...
+            //el.setAttribute(attr.props, attr.value)
+            el.setAttribute(Object.entries(attr)[0][0], Object.entries(attr)[0][1])
+        }
+    })
+    return el;
+}
 
-            // Зробити, щоб ми отримували точно сам об.
-            if (typeof attr === "object" && attr !== null) {
-                // отримати Значення з об...
-                for (const [key, value] of Object.entries(attr)) {
-                    el.setAttribute(key, value)
-                }
-            }
-        })
-        return el;
+export function createInputSring (type = "text", value = "", id, key) {
+    const input = `
+    <div class="element-product">
+      <label for="${id}">${value}</label>
+      <input type="${type}" id="${id}" data-type="${key}">
+    </div>
+    `
+    return input
+}
+
+export function createEditProductInput(p, v) {
+    const div = createHTMLElement("div", undefined);
+    const id = generationId();
+    const label = createHTMLElement("label", undefined, p, [{ for: id }]);
+    const input = createHTMLElement("input", undefined, v);
+    input.key = p;
+    if (p === "status") {
+        input.type = "checkbox"
+    }
+    if (p === "id" || p === "date" || p === "status") {
+        input.disabled = true;
+    }
+    input.id = id;
+
+    div.append(label, input)
+    return div
 }
