eabbc325958491382a4f2d54f0e3b4d1a4fc5d3b
marcik357 <marcik357@gmail.com>
2023-03-07 14:57:22 +0200

    Update: var.js - add btns

--- JS-14__itea0410crm/code/var.js
+++ JS-14__itea0410crm/code/var.js
@@ -1,4 +1,8 @@
-export const dayEN = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-export const dayUK = ["Неділя", "Понеділок","Вівторок","Середа","Четвер","П’ятниця","Субота"];
+import { createHTMLElement } from "./functions.js";
+
+export const dayEN = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+export const dayUK = ["Неділя", "Понеділок", "Вівторок", "Середа", "Четвер", "П’ятниця", "Субота"];
 export const getLogin = `${dayEN[new Date().getDay()]}${new Date().getHours()}`.toLowerCase();
 export const getPassword = `${dayUK[new Date().getDay()]}${new Date().getHours()}${new Date().getMinutes()}${new Date().getFullYear()}`.toLowerCase();
+export const modalSave = createHTMLElement("button", "btn-save", "Зберегти", [{ "type": "button" }, { "data-type": "button" }]);
+export const modalClose = createHTMLElement("button", "btn-close", "Скасувати", [{ "type": "button" }, { "data-type": "button" }]);
