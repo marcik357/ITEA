b2fc7d6afe73ab17f9e852a0886b3db86d1c5f13
marcik357 <marcik357@gmail.com>
2023-03-07 14:51:13 +0200

    Update: index.js - add try...catch for start page

--- JS-14__itea0410crm/code/index.js
+++ JS-14__itea0410crm/code/index.js
@@ -1,6 +1,6 @@
 import { getLogin, getPassword } from "./var.js";
-import { changeInputEvent, userLoginEvent, showModalEvent } from "./events.js";
-import { createHTMLElement } from "./functions.js";
+import { changeInputEvent, userLoginEvent, showModalEvent, hideModalEvent, saveData } from "./events.js";
+import { createHTMLElement, categorySelect } from "./functions.js";
 
 if (!sessionStorage.isLogin && !document.location.pathname.includes("/authorization")) {
     document.location = "/authorization";
@@ -12,30 +12,53 @@ try {
 
     document.getElementById("disabled")
         .addEventListener("click", userLoginEvent)
-}catch (error){
-   // console.error(error)
+} catch (error) {
+    // console.error(error)
 }
 
 
 //main page
+try {
+    document.querySelector(".add")
+        .addEventListener("click", showModalEvent);
 
-const btn = document.querySelector(".add")
-.addEventListener("click", showModalEvent);
-const modalWindow = document.querySelector(".container-modal .modal");
+    const modalWindow = document.querySelector(".container-modal .modal");
 
-const modalSave = createHTMLElement("button", "btn-save", "Зберегти", [{"type":"button"},{"data-type":"button"}]);
-const modalClose = createHTMLElement("button", "btn-close", "Скасувати", [{"type":"button"},{"data-type":"button"}]);;
+    const modalSave = createHTMLElement("button", "btn-save", "Зберегти", [{ "type": "button" }, { "data-type": "button" }]);
+    const modalClose = createHTMLElement("button", "btn-close", "Скасувати", [{ "type": "button" }, { "data-type": "button" }]);;
 
-modalWindow.insertAdjacentHTML("beforeend", `
-<h2>Додайте новий продукт до БД</h2>
-<div class="modal__body"></div>
-<div class="modal__control"></div>
-`)
+    modalWindow.insertAdjacentHTML("beforeend", `
+    <h2>Додайте новий продукт до БД</h2>
+    <div class="catigoty"></div>
+    <div class="modal__body"></div>
+    <div class="modal__control"></div>
+    `)
 
-document.querySelector(".modal__control").append(modalSave, modalClose)
+    document.querySelector(".modal__control").append(modalSave, modalClose);
+    categorySelect();
+
+    modalClose.addEventListener("click", hideModalEvent);
+
+    modalSave.addEventListener("click", saveData)
+
+    /*
+    Ваші події 
+    */
+
+} catch (e) {
+    console.error(e)
+}
 
 
+console.log(getLogin, getPassword);
 
-console.log(getLogin)
-console.log(getPassword)
+if(!localStorage.store){
+    localStorage.store = JSON.stringify([])
+}
+if(!localStorage.video){
+    localStorage.video = JSON.stringify([])
+}
+if(!localStorage.rest){
+    localStorage.rest = JSON.stringify([])
+}
 
