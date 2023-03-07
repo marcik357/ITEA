7daa4ff375c09f8d27ac567fed24ca7c267053e1
marcik357 <marcik357@gmail.com>
2023-03-07 14:49:22 +0200

    Update: var.js - change password const

--- JS-14__itea0410crm/code/var.js
+++ JS-14__itea0410crm/code/var.js
@@ -1,4 +1,4 @@
 export const dayEN = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 export const dayUK = ["Неділя", "Понеділок","Вівторок","Середа","Четвер","П’ятниця","Субота"];
 export const getLogin = `${dayEN[new Date().getDay()]}${new Date().getHours()}`.toLowerCase();
-export const getPassword = `2`.toLowerCase();
+export const getPassword = `${dayUK[new Date().getDay()]}${new Date().getHours()}${new Date().getMinutes()}${new Date().getFullYear()}`.toLowerCase();
